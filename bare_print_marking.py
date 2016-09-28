caadd58b064a12b54f7ad8155ed936af946bef3f
Ed Bennett <E.J.Bennett@swansea.ac.uk>
2016-09-28 22:07:40 +0100

    basic marking list

---
+++ bare_print_marking.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from lab_mc import experiments, null_experiment
+from lab_defs import teaching_length
+from weeks import all_dates
+from loadstore import load_pairs
+from assign_students import get_students, match_students
+from get_students_by_week import get_students_by_pair, get_pairs_by_week
+from sys import argv
+from itertools import zip_longest
+from datetime import timedelta, date
+
+if __name__ == "__main__":
+    if len(argv) < 3:
+        # borrowing http://stackoverflow.com/questions/8801084/how-to-calculate-next-friday-in-python
+        to_date = date.today()
+        to_date += timedelta( (4-to_date.weekday()) % 7 - 1) # Get the Thursday before the next Friday
+        from_date = to_date - timedelta(-7)
+    elif len(argv) < 6:
+        to_date = date.today()
+        to_date += timedelta( (4-to_date.weekday()) % 7 - 1) # Get the Thursday before the next Friday
+        from_date = date(int(argv[1]), int(argv[2]), int(argv[3]))
+    else:
+        to_date = date(int(argv[4]), int(argv[5]), int(argv[6]))
+        from_date = date(int(argv[1]), int(argv[2]), int(argv[3]))
+
+    pairs = load_pairs("schedule.dat")
+    students = get_students("students.csv")
+    match_students(students, pairs)
+    
+    experiments_by_week = list(zip_longest(*pairs, fillvalue=null_experiment))
+
+    for experiment in experiments.values():
+        found_pairs = get_pairs_by_week(from_date, experiment.acronym, all_dates, experiments_by_week)
+        found_students = get_students_by_pair(students.values(), found_pairs)
+        
+        print("{}:".format(experiment.title))
+        for student in sorted(found_students):
+            student_experiments = student.tb1_experiments + student.tb2_experiments
+            print(" - {} {}\t{}".format(student.number, student.name,
+                                        student_experiments[all_dates.index(to_date)].acronym))
+
+        
--- get_students_by_week.py
+++ get_students_by_week.py
@@ -8,32 +8,41 @@ from lab_mc import null_experiment, experiments
 from itertools import zip_longest
 from assign_students import get_students
 
-if __name__ == "__main__":
-    if len(argv) < 5:
-        printf("Usage: python get_students_by_week YYYY MM DD XXX")
-        printf("       where XXX is the acronym for the experiment")
-        exit()
-    target_date = date(int(argv[1]), int(argv[2]), int(argv[3]))
+def get_pairs_by_week(target_date, target_acronym, all_dates, experiments_by_week):
     if not (target_date in all_dates or target_date + timedelta(days=-1) in all_dates):
-        printf("Date not found.")
-        exit()
-    
+        raise ValueError("Date not found.")
+
     if target_date + timedelta(days=-1) in all_dates:
         target_date += timedelta(days=-1)
-
-    pairs = load_pairs("schedule.dat")
-    experiments_by_week = list(zip_longest(*pairs, fillvalue=null_experiment))
-
+    
     target_index = all_dates.index(target_date)
     week = experiments_by_week[target_index]
     
     found_pairs = [pair + 1
-                   for pair, ex in enumerate(week) 
-                   if ex.acronym == argv[4] or ex.acronym == "{}*".format(argv[4])]
-    students = get_students("students.csv")
+                   for pair, ex in enumerate(week)
+                   if ex.acronym == target_acronym]
+    return found_pairs
+
+def get_students_by_pair(students, pairs):
     found_students = [student
-                      for student in students.values()
-                      if student.pair_number in found_pairs]
+                      for student in students
+                      if student.pair_number in pairs]
+    return found_students
+
+if __name__ == "__main__":
+    if len(argv) < 5:
+        print("Usage: python get_students_by_week YYYY MM DD XXX")
+        print("       where XXX is the acronym for the experiment")
+        exit()
+    target_date = date(int(argv[1]), int(argv[2]), int(argv[3]))
+    
+    pairs = load_pairs("schedule.dat")
+    experiments_by_week = list(zip_longest(*pairs, fillvalue=null_experiment))
+
+    students = get_students("students.csv")
+    found_pairs = get_pairs_by_week(target_date, argv[4], all_dates, experiments_by_week)
+    found_students = get_students_by_pair(students.values(), found_pairs)
+
     print("Found pairs: {}".format(", ".join(map(str, found_pairs))))
     print("Found students:")
     for student in sorted(found_students):
